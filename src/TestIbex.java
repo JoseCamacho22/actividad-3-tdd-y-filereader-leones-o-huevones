228aaa49b4c6ae926e2aefc201b2c399379063fc
Roizus <alvaroroizo@gmail.com>
2015-11-11 18:23:01 +0100

    Actualizo mi rama con TDD

--- src/TestIbex.java
+++ src/TestIbex.java
@@ -1,12 +1,53 @@
+
 import static org.junit.Assert.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.junit.Test;
 
-public class TestIbex {
 
+public class TestIbex {
+	
+	final static String ruta = "res/bolsa.csv";
+	Ibex ibex=new Ibex();
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testFechaNoExisteGetValueALVARO() {
+		Float resultado = 0.0f;
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+		String sFecha = "00000000";
+		Date dFecha = null;
+		try {
+			dFecha = sdf.parse(sFecha);
+		} catch (ParseException e) {
+			
+		}
+		assertEquals(resultado, Ibex.getValue(dFecha,7, ruta ),0.0001);
+	}
+	
+	@Test
+	public void testFechaNullGetValueALVARO(){
+		float resultado= 0.0f;
+		assertEquals(resultado, Ibex.getValue(null,7, ruta ),0.0001);
+
+	}
+	
+	@Test
+	public void testFechaExisteGetValueALVARO(){
+		float resultado= 11206.6f;
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+		String sFecha = "20000104";
+		Date dFecha = null;
+		try {
+			dFecha = sdf.parse(sFecha);
+		} catch (ParseException e) {
+			
+		}
+		assertEquals(resultado, 
+				Ibex.getValue(dFecha,7, ruta ),0.1);
+
 	}
 
 }
+	
